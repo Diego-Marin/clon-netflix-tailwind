d3a6f4e39bab3c717cd6661499d4143211d2787d
Diego Mejia <diego.mejia@pascualbravo.edu.co>
2023-08-28 22:38:49 -0500

    Repositorio iniciar de la primera fase de desarrollo del proyecto clon netflix con Tailwin

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        '150': "500px"
+      },
+    },
+    plugins: [],
+  }
+}
